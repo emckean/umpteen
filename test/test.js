835c2ae053cb2fc249345ee57ac3d41001739053
Erin McKean <erin@wordnik.com>
2014-01-17 21:06:42 -0800

    working through popping numbers from end of the array

--- test/test.js
+++ test/test.js
@@ -6,11 +6,11 @@ describe('checking', function(){
 	describe('simpleCheck', function(){
 	  	var number1 = 1;
 	    it('should return "one"', function(){
-	      should.equal("one", umpteen.singleDigit(number1));
+	      should.equal("one", umpteen.underTwenty(number1));
 		})
 		var number2 = 2;
 	    it('should return "two"', function(){
-	      should.equal("two", umpteen.singleDigit(number2));
+	      should.equal("two", umpteen.underTwenty(number2));
 		})
 		var numberDecimal1 = 3.14;
 	    it('should return a simple number for a decimal', function(){
@@ -18,19 +18,51 @@ describe('checking', function(){
 		})
 	    var numberSpaces = '3 3';
 	    it('should return a number without spaces', function(){
-	      should.equal(33, umpteen.noSpaces(numberSpaces));
+	      should.equal(33, umpteen.onlyDigits(numberSpaces));
+		})
+	    var leadingNumberSpaces = ' 3 3';
+	    it('should return a number without spaces', function(){
+	      should.equal(33, umpteen.onlyDigits(numberSpaces));
 		})
 		var numberCommas = '3,003';
 	    it('should return a number without commas', function(){
-	      should.equal(3003, umpteen.noCommas(numberCommas));
+	      should.equal(3003, umpteen.onlyDigits(numberCommas));
+		})
+		var numberWithJunk = '9a*&^(*(&^8$$$';
+		it('should return just a number with no letters or symbols', function(){
+			should.equal(98, umpteen.onlyDigits(numberWithJunk));
 		})
 		var numberForArray = 30003;
 	    it('should return an array', function(){
 	      should.deepEqual([3,0,0,0,3], umpteen.arrayify(numberForArray));
 		})
-		var testNumber = 33;
-	    it('while loop should work', function(){
-	      should.equal(3, umpteen.chugNumbers(testNumber));
+		var number19 = 19;
+	    it('checking chugNumbers', function(){
+	    	console.log(umpteen.chugNumbers(number19));
+	      should.equal("nineteen", umpteen.chugNumbers(number19));
+	  	})
+	  	var testNumber19 = 19;
+	  	it ('should return teen numbers', function(){
+	  		should.equal("nineteen", umpteen.underTwenty(testNumber19))
+	  	})
+	  	var number33 = 33;
+	  	it ('should return thirty-three from 33', function(){
+	  		console.log(umpteen.chugNumbers(number33));
+	  		should.deepEqual(['three', 'thirty-'], umpteen.chugNumbers(number33))
+	  	})
+	  	var number123 = 123;
+	  	it ('should return three-digit slice of array', function(){
+	  		console.log(umpteen.sliceArray(number123));
+	  		should.deepEqual(["one", "twenty-", "three"], umpteen.sliceArray(number123))
+	  	})	  	
+	  	var number5678 = 5678;
+	  	it ('should return three-digit slice of array', function(){
+	  		console.log(umpteen.sliceArray(number5678));
+	  		should.deepEqual([8,7,6,5], umpteen.sliceArray(number5678))
+	  	})		  	
+	  	var reallyLongNumber = 10000000000000;
+	  	it('should reject too-long numbers', function(){
+	  	  should.equal("Sorry, nine trillion is as high as we go!", umpteen.checkLength(reallyLongNumber));
 	  	})
 	})
 })
--- umpteen.js
+++ umpteen.js
@@ -1,22 +1,28 @@
-var ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine','ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+var oneToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
     tens = ['', 'ten', 'twenty-', 'thirty-', 'fourty-', 'fifty-', 'sixty-', 'seventy-', 'eighty-', 'ninety-'];
+    ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
         var noDecimals = function(number){
             return Math.floor(number);
         }
-        var noSpaces = function(number){
-            var noSpace = number.replace(" ","");
-            return noSpace;
+
+        var onlyDigits = function(number){
+            var stringNum = number.toString();
+            var exp = /[^\d]/ig;
+            var justDigits = stringNum.replace(exp,"");
+            return justDigits;
         }
 
-        var noCommas = function(number){
-            var noComma = number.replace(",","");
-            return noComma;
+        var underTwenty = function(number) {
+            return oneToNineteen[number];
         }
 
+        var underHundred = function(number) {
+            return tens[number];
+        }
         var singleDigit = function(number) {
             return ones[number];
-        }
+        }       
 
         var arrayify = function(number) {
             var stringNum = number.toString();
@@ -26,20 +32,73 @@ var ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eigh
             return arrayOfNums;
         }
 
-        var chugNumbers = function(number) {
-            var arrayOfNums = arrayify(number);
-            var length =  arrayOfNums.length;
-            while (length > 0) {
-                return(arrayOfNums[1]);
+        var checkLength = function(number) {
+            var myNumArray = arrayify(number);
+            if (myNumArray.length > 13) {
+                return("Sorry, nine trillion is as high as we go!");
             }
         }
+
+        var chugNumbers = function(number) {
+            var spelledNums = [];
+            var arrayOfNums = arrayify(number);
+            var backwardsArrayOfNums = arrayOfNums.reverse();
+            var length = arrayOfNums.length;
+             if ((backwardsArrayOfNums[1] !== undefined) && (backwardsArrayOfNums[1] === 1)){ 
+                    var teenNum = backwardsArrayOfNums[1].toString() + backwardsArrayOfNums[0].toString();
+                    spelledNums.push(underTwenty(+teenNum));
+                    
+              }
+             else {
+                // onesPlace = backwardsArrayOfNums[0];
+                spelledNums.push(underTwenty(backwardsArrayOfNums[0]));
+                spelledNums.push(underHundred(backwardsArrayOfNums[1]));
+             } 
+          return spelledNums;
+
+        }
+
+    var sliceArray = function(number) {
+            var spelledNums = [];
+            var tempNums = [];
+            var arrayOfNums = arrayify(number);
+            // var backwardsArrayOfNums = arrayOfNums.reverse();
+            // var length = backwardsArrayOfNums.length;
+             
+
+    //okay, next bit is to check for undefined for tempNums[i], maybe ternary operator here? 
+          while (arrayOfNums.length > 0) {
+              for(var i=0; i<3; i++)  {
+                tempNums[i] = arrayOfNums.pop();
+            }
+
+              if ((tempNums[1] !== undefined) && (tempNums[1] === 1)){ 
+                    var teenNum = tempNums[1].toString() + tempNums[0].toString();
+                    spelledNums.push(underTwenty(+teenNum));
+                    
+              }
+             else {
+                spelledNums.push(underTwenty(tempNums[0]));
+                spelledNums.push(underHundred(tempNums[1]));
+             } 
+             if (tempNums[2] !== undefined) {
+                spelledNums.push(singleDigit(tempNums[2]));
+             }
+            }
+            return spelledNums.reverse();
+
+        }
+
 module.exports = {
     arrayify : arrayify,
     chugNumbers : chugNumbers,
-    singleDigit : singleDigit, 
-    noCommas : noCommas,
-    noSpaces : noSpaces,
-    noDecimals: noDecimals
+    underTwenty: underTwenty,
+    noDecimals: noDecimals,
+    onlyDigits: onlyDigits,
+    checkLength: checkLength,
+    singleDigit: singleDigit,
+    underHundred: underHundred,
+    sliceArray: sliceArray
 }
 // exports.noDecimals = function(number){
 //     return Math.floor(number);
